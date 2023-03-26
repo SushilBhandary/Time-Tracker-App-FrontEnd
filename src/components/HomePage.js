3a8dd6d6ec13fe4fe356752e0fc6c3327f3d0541
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-03-26 22:57:34 +0530

    updating home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,24 @@
-
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+import Task from "./Task";
+import Header from "./Header";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  useEffect( () => {
+    const data = JSON.parse(localStorage.getItem("jwt"))
+    if (!data) {
+      navigate('/login')
+    }
+  })
+
+
   return (
-    <div>HomePage</div>
+    <div className="flex justify-between p-10">
+      <Header />
+      <Task/>
+    </div>
   )
 }
 
