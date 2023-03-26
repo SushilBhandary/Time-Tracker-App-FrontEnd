3625f0c94e142a065d578b527aab73bc9d400833
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-03-26 22:58:31 +0530

    bug fix

--- src/components/signup.js
+++ src/components/signup.js
@@ -78,16 +78,17 @@ const Signup = () =>{
          }, 4000);
       }
     })
-    .catch(e => toast.error(e.response.data.error, {
-      position: "top-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "colored",
-      }))
+    .catch(e =>
+      toast.error(e.response.data.message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        }))
     
   }
 
