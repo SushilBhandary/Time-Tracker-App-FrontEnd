6d8c4e50c9c78b32d1ec3a7d81180b5794404814
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-03-27 20:55:44 +0530

    add context

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-
+import { UserContext } from '../context/context'
+import {useContext} from "react"
 
 const Header = () => {
+  const user = useContext(UserContext);
+
   return (
     <div className="bg-cyan-500 w-[25vw] h-[90vh] rounded-lg flex flex-col justify-around items-center text-2xl">
         <div>
           <div class="relative md:w-36 md:h-36 w-12 h-12 bg-gradient-to-r from-[#833ab4] to-[#1dc0fd] rounded-full flex justify-center items-center text-center p-5 shadow-xl mr-4 md:text-4xl  text-xl text-white ">
-            S
+            {user.name}
           </div>
           <h1 className="text-center"> UserName </h1>
         </div>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,23 +2,27 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import Task from "./Task";
 import Header from "./Header";
+import { UserContext } from '../context/context'
 
 function HomePage() {
   const navigate = useNavigate();
+  const data = JSON.parse(localStorage.getItem("jwt"))
 
   useEffect( () => {
-    const data = JSON.parse(localStorage.getItem("jwt"))
     if (!data) {
       navigate('/login')
     }
+
   })
 
 
   return (
-    <div className="flex justify-between p-10">
-      <Header />
-      <Task/>
-    </div>
+    <UserContext.Provider value={data.user}>
+      <div className="flex justify-between p-10">
+        <Header />
+        <Task/>
+      </div>
+    </UserContext.Provider>
   )
 }
 
